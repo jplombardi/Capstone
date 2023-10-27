fa57660a835d38e0202492520aca5621b555befa
jplombardi <25209167+jplombardi@users.noreply.github.com>
2023-10-26 20:42:10 -0400

    test1

---
+++ Assets/test.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public GameObject test2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myfunc();
+        
+    }
+
+    // Anything physics related
+    void FixedUpdate()
+    {
+        
+    }
+
+    void myfunc()
+    {
+        //etc.
+    }
+}
